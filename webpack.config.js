22770317d8144ba5b732fd5600bc829cbc391b17
Brenda Means <bjmeansjd@gmail.com>
2017-12-01 15:29:43 -0800

    c

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,15 @@ module.exports = {
     entry: './source/react-main.js',
     output: {path: __dirname, filename: 'bundle.js'},
     module: {
-        loaders: [
-            {
-                test: /.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {presets: ['env', 'react']}
+        loaders: [{
+            test: /.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['env', 'stage-0', 'react']
             }
-        ]
+        }]
+
     },
     devtool: "source-map"
 };
